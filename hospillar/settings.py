33372e266d5b70812955d28907159f71c0924920
The White Hat KE <austinkings30@gmail.com>
2023-09-14 01:34:37 +0300

    updates settings.py

--- hospillar/settings.py
+++ hospillar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4=qgl-uv5$l(t21cn6cs3&iy72pdzx(2_55!++0(zakw5no_)+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
