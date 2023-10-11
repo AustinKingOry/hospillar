3b150c901d210f14880ffa81ce883716971401f8
The White Hat KE <austinkings30@gmail.com>
2023-10-12 02:50:53 +0300

    update settings.py

--- hospillar/settings.py
+++ hospillar/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/img/'
 # commented out for whitenoise
 # STATICFILES_DIRS = [
 #     BASE_DIR / 'static',
 # ]
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'static/img'
 
 # added for whitenoise
 STATIC_ROOT = BASE_DIR / 'static'
